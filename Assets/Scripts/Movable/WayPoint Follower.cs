6f906fef52b775c12345d9e84fc1e1b7d066191c
Hnah <hanh113004@gmail.com>
2025-11-02 15:00:51 +0700

    Squashed commit of the following:
    commit e1b2aa84a250bf967369253434a9751995edbafa
    Merge: 278e267 128b82a
    Author: minh240804 <56783968+minh240804@users.noreply.github.com>
    Date:   Sun Nov 2 14:52:58 2025 +0700
    
        Merge branch 'haanh' of https://github.com/hn4h/PRU213_FA25 into haanh
    
    commit 278e2673db65f87957f2f99b6ec132094fea8316
    Author: minh240804 <56783968+minh240804@users.noreply.github.com>
    Date:   Sun Nov 2 14:49:07 2025 +0700
    
        competele map game1
    
    commit 128b82ae026a533ef9a1a6d698633c3a8474d76f
    Author: anhtth20 <anhtth.it.work@gmail.com>
    Date:   Sun Nov 2 01:36:33 2025 +0700
    
        something i dont know
    
    commit fa9b6fac80fe4c75f30b8363f4a45648630825c0
    Merge: a97185c f92a01e
    Author: minh240804 <56783968+minh240804@users.noreply.github.com>
    Date:   Sat Nov 1 21:27:55 2025 +0700
    
        Merge branch 'haanh' of https://github.com/hn4h/PRU213_FA25 into haanh
    
    commit a97185c271d40678ecb3ff681f10d3ee26e8738b
    Author: minh240804 <56783968+minh240804@users.noreply.github.com>
    Date:   Sat Nov 1 21:27:52 2025 +0700
    
        update saw lv1
    
    commit f92a01eea0c344f19dd0b773c06f33b59c9c1331
    Author: anhtth20 <anhtth.it.work@gmail.com>
    Date:   Sat Nov 1 18:27:13 2025 +0700
    
        delete lv1
    
    commit e16db5192841f2806ad94abe9b760cc99383d364
    Author: anhtth20 <anhtth.it.work@gmail.com>
    Date:   Sat Nov 1 17:10:30 2025 +0700
    
        add pause button and end scene login (chua chay duoc)
    
    commit ec2567a766f66e8ab953932ec8b1874dc5287448
    Author: anhtth20 <anhtth.it.work@gmail.com>
    Date:   Sat Nov 1 00:16:14 2025 +0700
    
        update level2 order layer
    
    commit 4af7cc522d538b818ace2e2726f244191c83b614
    Author: anhtth20 <anhtth.it.work@gmail.com>
    Date:   Fri Oct 31 17:20:03 2025 +0700
    
        update level 2.2
    
    commit c4f99ed039a5bf5da332949ad7dba3f14a6eab2a
    Author: anhtth20 <anhtth.it.work@gmail.com>
    Date:   Tue Oct 28 23:58:11 2025 +0700
    
        Update level2.unity
    
    commit 34b5677cccdfb2223b8eae34064ea27b2e91eb52
    Merge: 8fddf6f b38a8c5
    Author: anhtth20 <anhtth.it.work@gmail.com>
    Date:   Tue Oct 28 23:52:42 2025 +0700
    
        Merge branch 'demoSence' into haanh
    
    commit 8fddf6fa3dce94b4d8a1f1779f9dea3ed14362e9
    Author: anhtth20 <anhtth.it.work@gmail.com>
    Date:   Tue Oct 28 23:50:45 2025 +0700
    
        complete level2 scene

--- Assets/Scripts/Movable/WayPoint Follower.cs
+++ Assets/Scripts/Movable/WayPoint Follower.cs
@@ -16,19 +16,21 @@ public class WayPointFollower : MonoBehaviour
 
     private int currentPointIndex = 0;
     private bool isMovingForward = true;
-    
-    private void Update() {
-        if (Vector2.Distance(wayPoints[currentPointIndex].transform.position,transform.position) < 0.1f)
+
+    private void Update()
+    {
+        if (Vector2.Distance(wayPoints[currentPointIndex].transform.position, transform.position) < 0.1f)
         {
             if (direction == Direction.OneDirection)
             {
                 MoveOneDirection();
             }
-            else{
+            else
+            {
                 MoveTwoDirection();
             }
         }
-        transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentPointIndex].transform.position,speed*Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints[currentPointIndex].transform.position, speed * Time.deltaTime);
     }
 
     private void MoveOneDirection()
@@ -43,23 +45,23 @@ public class WayPointFollower : MonoBehaviour
     private void MoveTwoDirection()
     {
         if (isMovingForward)
+        {
+            currentPointIndex++;
+            if (currentPointIndex >= wayPoints.Length)
             {
-                currentPointIndex++;
-                if (currentPointIndex >= wayPoints.Length)
-                {
-                    currentPointIndex = wayPoints.Length - 1;
-                    isMovingForward = false; 
-                }
+                currentPointIndex = wayPoints.Length - 1;
+                isMovingForward = false;
             }
-            else
+        }
+        else
+        {
+            currentPointIndex--;
+            if (currentPointIndex < 0)
             {
-                currentPointIndex--;
-                if (currentPointIndex < 0)
-                {
-                    currentPointIndex = 0;
-                    isMovingForward = true; 
-                }
+                currentPointIndex = 0;
+                isMovingForward = true;
             }
+        }
     }
 
 }
